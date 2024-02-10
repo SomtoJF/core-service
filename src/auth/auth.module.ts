b2a050e5dd5240ff079aec37018f98ac5249d6f4
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-10 11:56:51 +0100

    register jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
