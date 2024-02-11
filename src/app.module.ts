fe2abbef037e6dd2549c350bc6404defd7d6e97c
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-11 15:08:12 +0100

    Import graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { GraphqlModule } from './graphql/graphql.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphqlModule,
+  ],
 })
 export class AppModule {}
