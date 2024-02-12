a660c732d843de75165ba2227d077c539de632e7
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-12 11:46:01 +0100

    Import custom scalar and exams module

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -5,12 +5,15 @@ import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { UsersModule } from './users/users.module';
+import { ExamsModule } from './exams/exams.module';
+import { DateTimeISOResolver } from 'graphql-scalars';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
+      resolvers: { Date: DateTimeISOResolver },
       definitions: {
         path: join(process.cwd(), 'src/graphql/graphql.ts'),
         outputAs: 'class',
@@ -20,6 +23,7 @@ import { UsersModule } from './users/users.module';
       playground: false,
     }),
     UsersModule,
+    ExamsModule,
   ],
   providers: [GraphqlService],
 })
