07acbc9052b888269f96fd1b83ea175746b5b0c4
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-18 17:42:36 +0100

    Delete login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,11 @@
 import { Controller, Post, Body, Res } from '@nestjs/common';
-import { LoginDto, SignupDto } from './dto';
+import { SignupDto } from './dto';
 import { AuthService } from './auth.service';
 import { Public } from 'src/decorators/public';
-import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
-  @Post('login')
-  @Public()
-  async login(@Body() dto: LoginDto, @Res() response: Response) {
-    const data = await this.authService.login(dto);
-    response.cookie('access_token', data.access_token, { httpOnly: true });
-    response.send(data.user).status(200);
-  }
 
   @Post('signup')
   @Public()
